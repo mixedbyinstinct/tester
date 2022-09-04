59afad1892ffa0e19e457760e8fba50077807c9f
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-09-04 18:18:59 -0400

    i think this might be it

--- notifyer.js
+++ notifyer.js
@@ -4,5 +4,15 @@ const spawn = require('child_process').spawn;
 const watcher = chokidar.watch('uploads', {ignored: /^\./, persistent: true});
 
 watcher.on('add', (path) => {
+    let logOut;
     console.log(path, ' has been added');
+    let process = spawn('python', ['mail.py']);
+
+    process.stdout.on('data', (data) => {
+        logOut = data.toString();
+    })
+    process.stdout.on('end', (code) => {
+        console.log(logOut);
+        console.log(`python process all stdio end with code ${code}`);
+    })
 })
