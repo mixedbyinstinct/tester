95a41f9f4771c2b5c1f87e349999c7a7b064096e
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-09-04 13:52:32 -0400

    notifyer again

--- notifyer.js
+++ notifyer.js
@@ -2,25 +2,27 @@ const MongoClient = require('mongodb').MongoClient;
 const spawn = require('child_process').spawn;
 const url = 'mongodb://localhost:27018/personal-site-db';
 
-MongoClient.connect(url, function(err, db) {
-    let log;
+const client = new MongoClient(url);
+
+let changeStream;
+async function run() {
     try {
-        if(err) {
-            throw err;
-        }
-        let watchCursor = db.getSiblingDB('personal-site-db').songs.watch();
-        while(!watchCursor.isClosed) {
-            let next = watchCursor.tryNext();
-            while(next !== null) {
-                let process = spawn('python', [mail.py]);
-                process.stdout.on('data', (data) => {
-                    log = data.toString();
-                })
-                process.stdout.on('end', () => console.log(log));
-                next = watchCursor.tryNext();
-            }
-        }
-    } catch(err) {
-        console.log(err);
+        const database = client.db('personal-site-db');
+        const collection = database.collection("songs");
+       
+        changeStream = collection.watch();
+        
+        changeStream.on("change", next => {
+            let log;
+            let process = spawn('python', ['mail.py']);
+            process.stdout.on('data', (data) => {
+                log = data.toString();
+            })
+            process.stdout.on('end', () => console.log(data));
+        })
+        await changeStream.close();
+    } finally {
+        await client.close();
     }
-})
+}
+run().catch(console.dir);
